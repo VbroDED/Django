67dccaeb8d4ac456d750b5b0510d3fbb8293ed4f
VbroDED <54136313+VbroDED@users.noreply.github.com>
2020-04-21 17:23:29 +0700

    start adding grades table

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,9 +1,12 @@
+# -*- coding: utf-8 -*-
+from django.views.generic.base import TemplateView
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.views.generic import View
 from .models import Post, Tag
 from .utils import ObjectDetailMixin
 from .forms import TagForm
+
 
 def posts_list(request):
     posts = Post.objects.all()
@@ -33,7 +36,48 @@ class TagCreate(View):
     def get(self, request):
         form = TagForm()
         return render(request, 'blog/tag_create.html', context={'form':form})
-        
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context.update(
+            {
+                'students_statistics': [
+                    {
+                        'id': 1,
+                        'fio': 'Someone',
+                        'timp': 2,
+                        'eis': 3,
+                        'philosophy': 4,
+                        'english': 5,
+                        'sport': 2.3,
+                        'average': 2.3,
+                    }
+                ],
+                'excellent_students': 'Student A, Student B',
+                'bad_students': 'Student C, Student D'
+            }
+        )
+        return context
+
+
+class Student:
+    pass
+
+
+class Statistics:
+    # student_id, [Scores]
+    pass
+
+class Subject:
+    pass
+
+class Score:
+    # Subject, Student, value
+    pass
+
 
 
 #    def get(self, request, slug):
